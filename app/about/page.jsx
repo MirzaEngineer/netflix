134b416c7704a40a21bb19171df07023aba8c4d0
MirzaEngineer <157305540+MirzaEngineer@users.noreply.github.com>
2025-06-28 00:45:40 -0700

    add name

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,6 +4,7 @@ import HeroSection from '../components/HeroSection'
 const About = () => {
   return (
     <>
+    <h1>Mirza Usama</h1>
     <HeroSection title = "OUR STORY" imageUrl='/about1.svg' />      
     </>
   )
