6d1d81df9bead743d875e161f4280b980138268b
MirzaEngineer <157305540+MirzaEngineer@users.noreply.github.com>
2025-06-28 00:51:28 -0700

    remove name

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,8 +4,6 @@ import HeroSection from '../components/HeroSection'
 const About = () => {
   return (
     <>
-    <h1>Mirza Usama</h1>
-    <p>Hello sir are you intrested</p>
     <HeroSection title = "OUR STORY" imageUrl='/about1.svg' />      
     </>
   )
